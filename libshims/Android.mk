ec66b1abd104ad4a9ec50b42039f16c91634ad87
Adrian DC <radian.dc@gmail.com>
2017-08-22 13:25:51 +0200

    msm8660-common: libshim: Upgrade library dependencies for AOSP 8.0.0
    Change-Id: I6973f8f4fd7009e1da845f8453104b74a255c3bb
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,7 +16,7 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES += \
     external/safe-iop/include \
     system/core/libutils
-LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libui libgui libbinder libutils libsync
+LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libui libgui libbinder libutils libsync libsensor
 LOCAL_MODULE := libshim_camera
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
